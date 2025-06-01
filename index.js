c6d8a524ef2f6da493723a2cdb6458ffd4710114
Khushi Jain <khushijain@Khushis-MBP-2.lan>
2025-06-01 22:45:47 +0530

    made some changes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('[#main]'),
+    el: document.querySelector('#main'),
     smooth: true
 });
+var a= document.querySelector("#elem1")
+var b=a.getAttribute("class")
+console.log(b)
